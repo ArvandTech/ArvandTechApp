a7b00996113c834341d1de92daa0f1c2b02d1446
Akshat <akshat.s.kaushik@student.uts.edu.au>
2019-02-18 20:12:17 +1100

    AWS Integration 0.11

--- app/src/main/java/com/arvandtechapp/MainActivity.java
+++ app/src/main/java/com/arvandtechapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout rellay1, rellay2;
