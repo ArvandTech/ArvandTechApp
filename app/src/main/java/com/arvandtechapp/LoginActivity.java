26493ce977177a0fdbaed1e466e04ba075ef0151
ArvandTech <operations@arvandtech.com.au>
2019-02-25 14:14:02 +1100

    Login 0.2

---
+++ app/src/main/java/com/arvandtechapp/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.arvandtechapp;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
+
+    //kkk
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable, 2000);
+    }
+
+}
--- app/src/main/java/com/arvandtechapp/MainActivity.java
+++ app/src/main/java/com/arvandtechapp/MainActivity.java
@@ -9,27 +9,5 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    RelativeLayout rellay1, rellay2;
-    Handler handler = new Handler();
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            rellay1.setVisibility(View.VISIBLE);
-            rellay2.setVisibility(View.VISIBLE);
-        }
-    };
 
-
-    //kkk
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
-        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
-
-        handler.postDelayed(runnable, 2000);
-    }
 }
